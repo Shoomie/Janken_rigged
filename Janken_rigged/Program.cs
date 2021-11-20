8a158e82399ef7344c54ebf245159b18c10c6e8e
Shoomie <93683368+Shoomie@users.noreply.github.com>
2021-11-20 03:33:36 +0100

    Add files via upload
    My first ever upload to Github, a totally rigged game of rock, paper scissor.

---
+++ Janken_rigged/Program.cs
@@ -0,0 +1,43 @@
+﻿class Janken_rigged
+{
+    static string input;
+    static void Main()
+    {
+        Janken();
+    }
+    static void Janken()
+    {
+        Console.Clear();
+        Console.WriteLine("Rock, paper or scissor?");
+        input = Console.ReadLine();
+        if (input == "rock")
+        {
+            Console.WriteLine("Paper, you lose.");
+        }
+        else if (input == "rock ")
+        {
+            Console.WriteLine("Scissors, you win.");
+        }
+        else if (input == "scissor")
+        {
+            Console.WriteLine("Rock, you lose");
+        }
+        else if (input == "scissor ")
+        {
+            Console.WriteLine("Paper, you win.");
+        }
+        else if (input == "paper")
+        {
+            Console.WriteLine("Scissor, you lose");
+        }
+        else if (input == "paper ")
+        {
+            Console.WriteLine("Rock, you win.");
+        }
+        else
+        {
+            Console.WriteLine("Please write a viable option.");
+        }
+        Janken();
+    }
+}
